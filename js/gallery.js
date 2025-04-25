bd3aaa381959c9744e29d4c26ff469ea1a2e8d6f
tech sreni <207423291+techsrenidemo@users.noreply.github.com>
2025-04-26 00:07:38 +0530

    Gallery page for demo added

---
+++ js/gallery.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const images = document.querySelectorAll(".img-fluid"); // Select all images
+    const modalImage = document.getElementById("modalImage");
+    const modalTitle = document.getElementById("imageModalLabel");
+  
+    images.forEach(img => {
+      img.addEventListener("click", () => {
+        const imgSrc = img.getAttribute("data-bs-image");
+        const caption = img.getAttribute("caption");
+  
+        modalImage.src = imgSrc;
+        modalImage.alt = caption;
+        modalTitle.textContent = caption; // Update modal header title
+      });
+    });
+  });
+  
